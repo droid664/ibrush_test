9d6bc7be829c8a54e1d77cd4dcaf49c151dd57c9
Droid664 <iskandarov.murat@gmail.com>
2024-05-15 03:08:09 +0500

    Слайдер с постами

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,32 @@
 import { customSelect } from './modules/customSelect'
+import Swiper from 'swiper'
+import { Grid, Pagination } from 'swiper/modules'
 
 document.addEventListener('DOMContentLoaded', () => {
     customSelect('[data-form-select]', 'Выберите книгу')
+
+    new Swiper('.similar-posts .swiper', {
+        slidesPerView: 3,
+        grid: {
+            rows: 2,
+        },
+        pagination: {
+            el: '.similar-posts .swiper-pagination',
+        },
+        breakpoints: {
+            0: {
+                slidesPerView: 1,
+                grid: false,
+                spaceBetween: 50,
+            },
+            768: {
+                slidesPerView: 3,
+                spaceBetween: 0,
+                grid: {
+                    rows: 2,
+                },
+            },
+        },
+        modules: [Grid, Pagination],
+    })
 })
